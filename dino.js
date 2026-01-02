699ee921beaf8fd6e964ee492e0f4ac8c73f8f7c
Chaitanya <chaitanyavarmalanke0.0@gmail.com>
2026-01-02 19:28:39 +0530

    your message

--- dino.js
+++ dino.js
@@ -2,38 +2,72 @@ import { getCustomProperty, setCustomProperty } from "./update.js"
 
 const dinoElem = document.querySelector("[data-dino]")
 
-/* ===== PHYSICS (Chrome-like) ===== */
+/* ===== PHYSICS ===== */
 const JUMP_FORCE = 0.6
 const GRAVITY = 0.0022
 
 /* ===== RUN ANIMATION ===== */
 const DINO_FRAME_COUNT = 2
-const FRAME_TIME = 120
+const FRAME_TIME = 120 // ms (lower = faster run)
 
+/* ===== STATE ===== */
 let isJumping
+let isGameOver
+let yVelocity
 let dinoFrame
 let frameTimer
-let yVelocity
-let isGameOver
 
 /* ================= SETUP ================= */
 export function setupDino() {
   isJumping = false
   isGameOver = false
+  yVelocity = 0
   dinoFrame = 0
   frameTimer = 0
-  yVelocity = 0
 
   setCustomProperty(dinoElem, "--bottom", 0)
-  dinoElem.src = "images/dino.png"
+  dinoElem.src = "./images/dino-run-0.png"
   dinoElem.style.setProperty("--dino-size", "36%")
 }
 
 /* ================= UPDATE ================= */
 export function updateDino(delta) {
   if (isGameOver) return
+
   handleRun(delta)
   handleJump(delta)
+}
+
+/* ================= RUN ================= */
+function handleRun(delta) {
+  if (isJumping) {
+    dinoElem.src = "./images/dino-stationary.png"
+    return
+  }
+
+  frameTimer += delta
+  if (frameTimer >= FRAME_TIME) {
+    dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT
+    dinoElem.src = `./images/dino-run-${dinoFrame}.png`
+    frameTimer -= FRAME_TIME
+  }
+}
+
+/* ================= JUMP ================= */
+function handleJump(delta) {
+  if (!isJumping) return
+
+  const bottom =
+    getCustomProperty(dinoElem, "--bottom") + yVelocity * delta
+  setCustomProperty(dinoElem, "--bottom", bottom)
+
+  yVelocity -= GRAVITY * delta
+
+  if (bottom <= 0) {
+    setCustomProperty(dinoElem, "--bottom", 0)
+    isJumping = false
+    yVelocity = 0
+  }
 }
 
 /* ================= HITBOX ================= */
@@ -54,42 +88,8 @@ export function setDinoLose() {
   yVelocity = 0
 
   setCustomProperty(dinoElem, "--bottom", 0)
-  dinoElem.src = "images/dino-lose.png"
+  dinoElem.src = "./images/dino-lose.png"
   dinoElem.style.setProperty("--dino-size", "37%")
-}
-
-/* ================= RUN ================= */
-function handleRun(delta) {
-  if (isJumping) {
-    dinoElem.src = "images/dino-stationary.png"
-    return
-  }
-
-  frameTimer += delta
-  if (frameTimer >= FRAME_TIME) {
-    dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT
-    dinoElem.src = `images/dino-run-${dinoFrame}.png`
-    frameTimer -= FRAME_TIME
-  }
-}
-
-/* ================= JUMP ================= */
-function handleJump(delta) {
-  if (!isJumping) return
-
-  const bottom =
-    getCustomProperty(dinoElem, "--bottom") + yVelocity * delta
-  setCustomProperty(dinoElem, "--bottom", bottom)
-
-  // gravity pull
-  yVelocity -= GRAVITY * delta
-
-  // landing
-  if (bottom <= 0) {
-    setCustomProperty(dinoElem, "--bottom", 0)
-    isJumping = false
-    yVelocity = 0
-  }
 }
 
 /* ================= INPUT ================= */
